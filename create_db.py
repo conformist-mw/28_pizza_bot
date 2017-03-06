843f89029acfdd30d9aef7298b69bd3ad4828e48
conformist-mw <conformist.mw@gmail.com>
2017-03-06 21:19:23 +0200

    added load catalog from json file; simplify classes assertion with unpack dict in class argument

--- create_db.py
+++ create_db.py
@@ -1,20 +1,23 @@
-from models import Base, Catalog, Choice, catalog
+from models import base_class, Catalog, Choice
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from os import getenv
+import json
 
 engine = create_engine(getenv('db_uri'))
-Base.metadata.create_all(engine)
+base_class.metadata.create_all(engine)
 session_class = sessionmaker(bind=engine)
 session = session_class()
+
+with open('catalog_fixtures.json', 'r') as json_file:
+    catalog = json.load(json_file)
+
 for item in catalog:
-    new_item = Catalog()
-    new_item.title = item['title']
-    new_item.description = item['description']
+    new_item = Catalog(
+        **{'title': item['title'], 'description': item['description']})
     for choice in item['choices']:
-        choices = Choice()
-        choices.title = choice['title']
-        choices.price = choice['price']
+        choices = Choice(
+            **{'title': choice['title'], 'price': choice['price']})
         new_item.choices.append(choices)
     session.add(new_item)
 session.commit()
